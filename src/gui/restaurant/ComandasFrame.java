69ee4e071361cf7a948d643cb810ae2e41d6707f
sperez <sperez@gilberto-PC>
2016-06-20 17:42:56 -0500

    avance 2016-06-20 1730

--- src/gui/restaurant/ComandasFrame.java
+++ src/gui/restaurant/ComandasFrame.java
@@ -191,9 +191,7 @@ public class ComandasFrame extends InternalFrameAbstracto {
 
             comandaPanel = comandas.get(i);
             posX += separatorX;
-            sum_width = posX + width_panel;
-                         
-            
+            sum_width = posX + width_panel;                                     
             
             if (sum_width +  separatorX > this.jScrollPane2.getWidth()-jScrollPane2.getVerticalScrollBar().getWidth()  && i != 0) {
 
